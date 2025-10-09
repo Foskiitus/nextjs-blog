bc13b9e20e7aa36619bb7d22599e4ac9d5801a1d
foskiitus@gmail.com <foskiitus@gmail.com>
2025-10-09 08:42:41 +0200

    feat: implement Container, PostsList, and SpinLoader components; update HomePage layout and add simulated loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import { Header } from '@/components/Header';
+import { Container } from '@/components/Container';
+import { PostsList } from '@/components/PostsList';
+import { SpinLoader } from '@/components/SpinLoader';
+import { Suspense } from 'react';
 
 // page.tsx (server) -> menu.tsx (server) -> link (client)
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Container>
+      <header>
+        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a header</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <p className='text-6xl font-bold text-center py-8'>Footer</p>
+      </footer>
+    </Container>
   );
 }
